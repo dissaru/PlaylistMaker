e88ca22f53334feae7a890194633e445c6cd7453
dissaru <nerounse@gmail.com>
2025-04-03 20:19:15 +0300

    .

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,13 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Switch
+import androidx.core.content.ContextCompat
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -15,6 +19,26 @@ class SettingsActivity : AppCompatActivity() {
 
         buttonBack.setOnClickListener {
             finish()
+        }
+        val switch = findViewById<SwitchMaterial>(R.id.switchDarkTheme)
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            // Получаем цвета из ресурсов
+            val thumbColor = if (isChecked) {
+                ContextCompat.getColor(this, R.color.switchBackgroundDark)
+            } else {
+                ContextCompat.getColor(this, R.color.switchThumbUnchecked)
+            }
+
+            val trackColor = if (isChecked) {
+                ContextCompat.getColor(this, R.color.switchThumbChecked)
+            } else {
+                ContextCompat.getColor(this, R.color.switchBackgroundLight)
+            }
+
+            // Применяем цвета
+            switch.thumbTintList = ColorStateList.valueOf(thumbColor)
+            switch.trackTintList = ColorStateList.valueOf(trackColor)
         }
     }
 }
